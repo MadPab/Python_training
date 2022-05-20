5c208c0e05bdbc8a8f375d7a94e138bbf12237e6
MadPab <45864411+MadPab@users.noreply.github.com>
2022-05-20 10:57:17 +0700

    delete pyt

--- Python_training/test/test_add_contact.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from Python_training.model.group2 import Group
-
-
-def test_add_contact(app):
-        app.group2.create(Group(firstname="as", lastname="asd", title="a", company="s", address="vbnvvn", home="cvv", mobile="543435"))
-
-def test_add_empty_contact(app):
-        app.group2.create(Group(firstname="", lastname="", title="", company="", address="", home="", mobile=""))
